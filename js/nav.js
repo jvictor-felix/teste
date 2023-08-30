82594542006b714fece938eaa3629ab58bd26383
João Victor <victoralmeida.va2@gmail.com>
2023-08-30 17:13:11 -0300

    subir arquivos testes do site

---
+++ js/nav.js
@@ -0,0 +1,47 @@
+
+const figure0 = document.querySelector(".one");
+const figure1 = document.querySelector(".two");
+const figure2 = document.querySelector(".three");
+
+/*function selecionado(number){
+    if(number == 0){
+        if(!figure0.classList.contains("selecionado")){
+            figure0.classList.add("selecionado");
+            figure1.classList.remove("selecionado");
+            figure2.classList.remove("selecionado");
+        }
+    }
+    if(number == 1){
+        if(!figure1.classList.contains("selecionado")){
+            figure0.classList.remove("selecionado");
+            figure1.classList.add("selecionado");
+            figure2.classList.remove("selecionado");
+        }
+    }
+    if(number == 2){
+        if(!figure2.classList.contains("selecionado")){
+            figure0.classList.remove("selecionado");
+            figure1.classList.remove("selecionado");
+            figure2.classList.add("selecionado");
+        }
+    }
+}
+*/
+
+function teste(classe, one, two, three){
+    if(!one.classList.contains(classe)){
+        one.classList.add(classe);
+        two.classList.remove(classe);
+        three.classList.remove(classe);
+    }
+   
+}
+
+
+function teste2(classe, one, two, three){
+    if(!document.querySelector(one).classList.contains(classe)){
+        document.querySelector(one).classList.add(classe);
+        document.querySelector(two).classList.remove(classe);
+        document.querySelector(three).classList.remove(classe);
+    }
+}
